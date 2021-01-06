ad9a62458f725f5be47e526d67fd338de6ca0b01
Vlad Timu <vlad.timu@smartboxdigital.com>
2021-01-06 18:53:09 +0200

    Laboratorul12

--- Controllers/CustomersGrpcController.cs
+++ Controllers/CustomersGrpcController.cs
@@ -33,5 +33,59 @@ namespace Timu_Vlad_lab2.Controllers
             }
             return View(customer);
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Customer customer = client.Get(new CustomerId() { Id = (int)id });
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Empty response = client.Delete(new CustomerId()
+            {
+                Id = id
+            });
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Customer customer = client.Get(new CustomerId() { Id = (int)id });
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                var client = new CustomerService.CustomerServiceClient(channel);
+                Customer response = client.Update(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
     }
 }
